86194e7bc1bab59abc77d6a7cbec46466b24c6f6
Harley de Brito <harleybrito@live.com>
2020-08-10 00:17:38 -0300

    testes com json-server

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  date: Date = new Date();
+
+  constructor(private http: HttpClient) {
+    // this.date.setHours(8);
+    // this.date.setMinutes(0);
+    // this.date.setSeconds(0);
+    this.http.get("http://192.168.0.105:3000/employee/0?_embed=scheduleEmployee").subscribe(
+      (response: any) => {
+        console.log(response);
+        let url = "http://192.168.0.105:3000/schedule/?";
+        response.scheduleEmployee.forEach((element: { scheduleId: number; }) => {
+          url = url.concat("id=" + element.scheduleId + "&");
+        });
+        this.http.get(url).subscribe(
+          (response) => console.log(response)
+        );
+      },
+      (response) => console.log(response.message)
+    );
+  }
 
   ngOnInit(): void {
+    console.log(this.date);
   }
 
 }
